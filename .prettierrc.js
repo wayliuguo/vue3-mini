1bd2344a7bdcd300ba5a95df3084db62d9730bbd
liuguowei <1085550655@qq.com>
2024-06-21 22:49:34 +0800

    feat: 接入prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: false, //强制在语句末尾不使用分号。
+    trailingComma: 'none', //不允许在多行结构的最后一个元素或属性后添加逗号。
+    singleQuote: true, //使用单引号而不是双引号来定义字符串。
+    printWidth: 120, //指定每行代码的最大字符宽度，超过这个宽度的代码将被换行
+    tabWidth: 4, //指定一个制表符（Tab）等于多少个空格。
+    singleQuote: true, //使用单引号
+    arrowParens: 'avoid', //  箭头函数括号
+    endOfLine: 'auto', // 结尾换行自动
+    jsxBracketSameLine: true
+}
