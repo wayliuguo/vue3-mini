352a61e4837c8f8b281a395a7695c6f944223c84
liuguowei <1085550655@qq.com>
2024-11-04 22:07:07 +0800

    feat: 实现 transform 功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,36 @@
+export function transform(root: any, options: any) {
+    const context = createTransformContext(root, options)
+    // 1. 遍历 - 深度优先搜索
+    traverseNode(root, context)
+    // 2. 修改 text content
+}
+
+// 创建全局对象, 存储源对象及插件列表
+function createTransformContext(root: any, options: any) {
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || []
+    }
+    return context
+}
+
+function traverseNode(node: any, context: any) {
+    // 遍历插件列表，分别使用插件进行转换
+    const nodeTransforms = context.nodeTransforms
+    for (let i = 0; i < nodeTransforms.length; i++) {
+        const transform = nodeTransforms[i]
+        transform(node)
+    }
+
+    traverseChildren(node, context)
+}
+
+function traverseChildren(node: any, context: any) {
+    const children = node.children
+    if (children) {
+        for (let i = 0; i < children.length; i++) {
+            const node = children[i]
+            traverseNode(node, context)
+        }
+    }
+}
